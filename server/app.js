c03fec07aa2075d8e494a0fccd09e9991371d380
Faruk Alibasic <faruk.alibasic@gmail.com>
2019-09-12 09:53:39 +0200

    remove 24 hour caching

--- server/app.js
+++ server/app.js
@@ -72,62 +72,43 @@ app.post('/events', function (req, res) {
   }
 
   if (req.body.url) {
-    if (EVENTS_DATA[req.body.url] && !isSyncThresholdCrossed) {
-      returnEventIndexFromCurrentDate(EVENTS_DATA[req.body.url], req.body.date, function (index) {
-        if (index != -1) {
-          paginatedListOfEvents = EVENTS_DATA[req.body.url].slice(offset + index, (offset + index + limit));
-          res.send({
-            'statusCode': 200,
-            'events': paginatedListOfEvents,
-            'totalEvents': EVENTS_DATA[req.body.url].length - index
-          });
-        } else {
-          res.send({
-            'statusCode': 404,
-            'events': null,
-            'totalEvents': 0
+    request(req.body.url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = ical2json.convert(body);
+        if (data && data.VEVENT && data.VEVENT.length) {
+          var mergedEvents
+          if (data && data.VCALENDAR && data.VCALENDAR[0] && data.VCALENDAR[0].VEVENT) mergedEvents = data.VCALENDAR[0].VEVENT.concat(data.VEVENT)
+          else mergedEvents = data.VEVENT
+          processData(mergedEvents, function (events) {
+            mergedEvents = events;
+            mergedEvents = mergedEvents.sort(function (a, b) {
+              return a.startDate - b.startDate;
+            });
+            EVENTS_DATA[req.body.url] = mergedEvents;
+            returnEventIndexFromCurrentDate(mergedEvents, req.body.date, function (index) {
+              if (index != -1) {
+                paginatedListOfEvents = mergedEvents.slice(offset + index, (offset + index + limit));
+                res.send({
+                  'statusCode': 200,
+                  'events': paginatedListOfEvents,
+                  'totalEvents': mergedEvents.length - index
+                });
+              } else {
+                res.send({
+                  'statusCode': 404,
+                  'events': null,
+                  'totalEvents': 0
+                });
+              }
+            });
           });
         }
-      });
-    }
-    else {
-      request(req.body.url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var data = ical2json.convert(body);
-          if (data && data.VEVENT && data.VEVENT.length) {
-            var mergedEvents
-            if (data && data.VCALENDAR && data.VCALENDAR[0] && data.VCALENDAR[0].VEVENT) mergedEvents = data.VCALENDAR[0].VEVENT.concat(data.VEVENT)
-            else mergedEvents = data.VEVENT
-            processData(mergedEvents, function (events) {
-              mergedEvents = events;
-              mergedEvents = mergedEvents.sort(function (a, b) {
-                return a.startDate - b.startDate;
-              });
-              EVENTS_DATA[req.body.url] = mergedEvents;
-              returnEventIndexFromCurrentDate(mergedEvents, req.body.date, function (index) {
-                if (index != -1) {
-                  paginatedListOfEvents = mergedEvents.slice(offset + index, (offset + index + limit));
-                  res.send({
-                    'statusCode': 200,
-                    'events': paginatedListOfEvents,
-                    'totalEvents': mergedEvents.length - index
-                  });
-                } else {
-                  res.send({
-                    'statusCode': 404,
-                    'events': null,
-                    'totalEvents': 0
-                  });
-                }
-              });
-            });
-          }
-          else
-            res.send({ 'statusCode': 404, 'events': null });
-        } else
-          res.send({ 'statusCode': 500, 'events': null });
-      });
-    }
+        else
+          res.send({ 'statusCode': 404, 'events': null });
+      } else
+        res.send({ 'statusCode': 500, 'events': null });
+    });
+
   } else
     res.send({ 'statusCode': 404, 'events': null });
 });
